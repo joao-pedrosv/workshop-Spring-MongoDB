6a1543f41e5087601178ad0d30e868cdc60ec120
joao-pedro7771 <joaaopedrodsv@gmail.com>
2025-08-27 18:18:01 -0300

    MongoDB connection, user service and repository

---
+++ src/main/java/com/example/workshopmongo/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.workshopmongo.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+
+}
